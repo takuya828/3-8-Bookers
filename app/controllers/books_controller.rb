87d3d3fe59eb1349d95d9c25fb01ed3b9c3f439c
takuya828 <takuya11070221@gmail.com>
2021-08-17 07:03:35 +0000

    4-8 books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,29 @@
 class BooksController < ApplicationController
+  
   def index
-    @books = Book.all
+     @books = Book.all
   end
   
   def show
   end
   
   def edit
+  end
+  
+  def new
+    @book = Book.new
+    
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/top'
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
   
 end
