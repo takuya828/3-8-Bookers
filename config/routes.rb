29a249277000124f9796425e301e8c5727bd0dc7
takuya828 <takuya11070221@gmail.com>
2021-08-17 15:14:23 +0000

    4-8 ../../config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  get 'books/new'
+  # get 'books/new'
   get '/' => 'homes#top'
-  get 'books' => 'books#index'
-  get 'books' => 'books#show'
-  get 'books' => 'books#edit'
-  post 'books' => 'books#create'
+  resources :books
+    patch 'books/:id' => 'books#update', as: 'update_book'
+    delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  # get 'books' => 'books#index'
+  # get 'books' => 'books#show'
+  # get 'books' => 'books#edit'
+  # post 'books' => 'books#create'
+  # get 'books/:id' => 'books#show', as: 'books'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
