88fba316420c6ce21331de5694de33110edf765d
takuya828 <takuya11070221@gmail.com>
2021-08-18 05:51:40 +0000

    4-8 books_controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,29 +19,29 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    pp "=============================================================================="
-    pp book_params
-    pp "=============================================================================="
-    book.save
-   redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+     flash[:success] = 'Book was successfully created.'
+     redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+     render "index"
+    end
   end
   def update
-     book = Book.find(params[:id])
-     pp "================================================================================="
-     pp book
-     pp "================================================================================"
-     pp params[:id]
-     pp "================================================================================"
-     book_params
-     pp "================================================================================"
-    book.update(book_params)
-    redirect_to book_path(book.id)
+     @book = Book.find(params[:id])
+     if @book.update(book_params)
+     flash[:update] = 'Book was successfully updated.'
+     redirect_to book_path(@book.id)
+     else
+       @books = Book.all
+       render "edit"
+     end
   end
   def destroy
-     book = Book.find(params[:id])  # データ（レコード）を1件取得
-    book.destroy  # データ（レコード）を削除
-    redirect_to books_path  # 投稿一覧画面へリダイレクト
+     book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
 
